a072d247a27deffbd228642f8a6582614aa30ab0
psagemo <psagemo@gmail.com>
2022-09-14 15:19:13 +0200

    Commented product class

--- AssetTracking/Product Class.cs
+++ AssetTracking/Product Class.cs
@@ -8,6 +8,7 @@ namespace AssetTracking
 {
     internal class Product
     {
+        // Product variables
         public string Type;
         public string Brand;
         public string Model;
@@ -17,7 +18,7 @@ namespace AssetTracking
         public string Currency;
         public double LocalPriceToday;
 
-
+        // Product constructor
         public Product(string type, string brand, string model, string office, DateTime purchaseDate, int uSD, string currency, double localPriceToday)
         {
             Type = type;
@@ -30,75 +31,29 @@ namespace AssetTracking
             LocalPriceToday = localPriceToday;
         }      
         
-        
+        // Print method
         public void PrintProduct()
         {
+            // Print product in red if purchase date is within 3 months from being 3 years old
             if (PurchaseDate.AddMonths(-3) < DateTime.Now.AddYears(-3))
             {
                 Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine(Type.PadRight(20) + Brand.PadRight(20) + Model.PadRight(20) + Office.PadRight(20) + PurchaseDate.ToString("MM/dd/yyyy").PadRight(20) + USD.ToString().PadRight(20) + Currency.PadRight(20) + LocalPriceToday); Console.ResetColor();
             }
+            
+            // Print product in yellow if purchase date is within 6 months from being 3 years old
             else if (PurchaseDate.AddMonths(-6) < DateTime.Now.AddYears(-3))
             {
                 Console.ForegroundColor = ConsoleColor.Yellow;
                 Console.WriteLine(Type.PadRight(20) + Brand.PadRight(20) + Model.PadRight(20) + Office.PadRight(20) + PurchaseDate.ToString("MM/dd/yyyy").PadRight(20) + USD.ToString().PadRight(20) + Currency.PadRight(20) + LocalPriceToday);
                 Console.ResetColor();
             }
+            
+            // Print product
             else
             {
                 Console.WriteLine(Type.PadRight(20) + Brand.PadRight(20) + Model.PadRight(20) + Office.PadRight(20) + PurchaseDate.ToString("MM/dd/yyyy").PadRight(20) + USD.ToString().PadRight(20) + Currency.PadRight(20) + LocalPriceToday);
             }
         }
     }
-   
-    //internal class Phone : Product
-    //{
-    //    string Type;
-    //    string Brand;
-    //    string Model;
-    //    string Office;
-    //    //public virtual new DateTime PurchaseDate;
-    //    int USD;
-    //    string Currency;
-    //    double LocalPriceToday;
-
-    //    public override DateTime PurchaseDate => throw new NotImplementedException();
-
-    //    public Phone(string type, string brand, string model, string office, DateTime purchaseDate, int uSD, string currency, double localPriceToday)
-    //    {
-    //        Type = type;
-    //        Brand = brand;
-    //        Model = model;
-    //        Office = office;
-    //        //PurchaseDate = purchaseDate;
-    //        USD = uSD;
-    //        Currency = currency;
-    //        LocalPriceToday = localPriceToday;
-    //    }
-    //}
-    //internal class Computer : Product
-    //{
-    //    string Type;
-    //    string Brand;
-    //    string Model;
-    //    string Office;
-    //    //DateTime PurchaseDate;
-    //    int USD;
-    //    string Currency;
-    //    double LocalPriceToday;
-
-    //    public Computer(string type, string brand, string model, string office, DateTime purchaseDate, int uSD, string currency, double localPriceToday)
-    //    {
-    //        Type = type;
-    //        Brand = brand;
-    //        Model = model;
-    //        Office = office;
-    //        //PurchaseDate = purchaseDate;
-    //        USD = uSD;
-    //        Currency = currency;
-    //        LocalPriceToday = localPriceToday;
-    //    }
-
-    //    public override DateTime PurchaseDate => throw new NotImplementedException();
-    //}
 }
